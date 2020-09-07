900c85999891ca3d1138c420e71fc762351462f3
fabihsuan <fabihsuan@gmail.com>
2020-09-07 22:06:55 +0800

    Update 450A - Jzzhu and Children.cpp
    add different solutions

--- 450A - Jzzhu and Children.cpp
+++ 450A - Jzzhu and Children.cpp
@@ -1,3 +1,4 @@
+//first version
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -27,3 +28,38 @@ int main() {
   cout << ans << endl;
   return 0;
 }
+//second version
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n, m, a;
+  cin >> n >> m;
+  vector<int> v;
+  for (int i = 1; i <= n; i++) {
+    cin >> a;
+    v.push_back(a);
+  }
+  int max1 = 0, max2, x, locate;
+  for (int i = 0; i < n; i++) {
+    x = v[i] / m;
+
+    if (v[i] % m != 0) {
+      max2 = x + 1;
+      if (max2 >= max1) {
+        max1 = max2;
+        locate = i;
+      }
+    } else if (v[i] % m == 0) {
+      max2 = x;
+      if (max2 >= max1) {
+        max1 = max2;
+        locate = i;
+      }
+    }
+  }
+  cout << locate + 1 << endl;
+  return 0;
+}
+
